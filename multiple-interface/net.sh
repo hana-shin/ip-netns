26da029b27844bd8989834f5350274e2f57a08f6
hana-shin <xxx@example.com>
2019-07-03 21:33:33 +0900

    modify

--- multiple-interface/net.sh
+++ multiple-interface/net.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/bash
 
-## add
 ip netns add cl
 ip netns add sv
 
-ip link add p-h1 type veth peer name p2-br
-ip link add p-h2 type veth peer name p3-br
+ip link add p0-sv type veth peer name p0-br
+ip link add p1-cl type veth peer name p1-br
 
-ip link set p-h1 netns cl
-ip link set p-h2 netns sv
+ip link set p0-sv netns sv
+ip link set p1-cl netns cl
 
 brctl addbr bridge
-brctl addif bridge p2-br
-brctl addif bridge p3-br
+brctl addif bridge p0-br
+brctl addif bridge p1-br
 
-ip netns exec cl ip addr add 10.0.0.10/8 dev p-h1
-ip netns exec sv ip addr add 10.0.0.20/8 dev p-h2
+ip netns exec sv ip addr add 10.0.0.10/8 dev p0-sv
+ip netns exec cl ip addr add 10.0.0.20/8 dev p1-cl
 
-ip netns exec cl ip link set p-h1 up
-ip netns exec sv ip link set p-h2 up
+ip netns exec sv ip link set p0-sv up
+ip netns exec cl ip link set p1-cl up
 
 ip netns exec cl ip link set lo up
 ip netns exec sv ip link set lo up
 
-ip link set p2-br up
-ip link set p3-br up
+ip link set p0-br up
+ip link set p1-br up
 ip link set bridge up
