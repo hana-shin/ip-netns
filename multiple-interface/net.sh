7721b7a2c41d00d359b9038c17bd3bf07e5d70bb
hana-shin <xxx@example.com>
2019-07-03 21:09:20 +0900

    modyfi

--- multiple-interface/net.sh
+++ multiple-interface/net.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/bash
 
-ip netns add host1
-ip netns add host2
+ip netns add cl
+ip netns add sv
 
 ip link add p-h1 type veth peer name p2-br
 ip link add p-h2 type veth peer name p3-br
 
-ip link set p-h1 netns host1
-ip link set p-h2 netns host2
+ip link set p-h1 netns cl
+ip link set p-h2 netns sv
 
 brctl addbr bridge
 brctl addif bridge p2-br
 brctl addif bridge p3-br
 
-ip netns exec host1 ip addr add 10.0.0.10/8 dev p-h1
-ip netns exec host2 ip addr add 10.0.0.20/8 dev p-h2
+ip netns exec cl ip addr add 10.0.0.10/8 dev p-h1
+ip netns exec sv ip addr add 10.0.0.20/8 dev p-h2
 
-ip netns exec host1 ip link set p-h1 up
-ip netns exec host2 ip link set p-h2 up
+ip netns exec cl ip link set p-h1 up
+ip netns exec sv ip link set p-h2 up
 
-ip netns exec host1 ip link set lo up
-ip netns exec host2 ip link set lo up
+ip netns exec cl ip link set lo up
+ip netns exec sv ip link set lo up
 
 ip link set p2-br up
 ip link set p3-br up
